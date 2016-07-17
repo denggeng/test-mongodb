f6bd0a38b58d8d5cb7c3e6f75dd93620d92225f1
dengg <dengg@DESKTOP-F0SG04C>
2016-07-17 23:42:47 +0800

    修改用户信息

--- src/main/java/com/test/account/controller/UserController.java
+++ src/main/java/com/test/account/controller/UserController.java
@@ -5,6 +5,7 @@ package com.test.account.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -21,8 +22,13 @@ public class UserController {
 
 	@Autowired
 	private UserService userService;
-	
-	
+
+	@RequestMapping("list")
+	public String list(Model model) {
+		Iterable<User> list = userService.findAll();
+		model.addAttribute("list", list);
+		return "account/user-list";
+	}
 
 	@RequestMapping("addSimpleUser")
 	@ResponseBody
