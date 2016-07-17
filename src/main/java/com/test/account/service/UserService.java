dc03219e7845c7ab63d619bb20ff79fe62a22056
GalaxyStar <denggeng2000@gmail.com>
2016-07-17 11:46:55 +0800

    加入密码编码

--- src/main/java/com/test/account/service/UserService.java
+++ src/main/java/com/test/account/service/UserService.java
@@ -4,27 +4,34 @@
 package com.test.account.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.test.account.domain.User;
 import com.test.account.repository.UserRepository;
+import com.test.framework.service.BaseService;
 
 /**
  * @author Kenny
  * @time 2016年7月14日 下午3:39:21
  */
 @Service
-public class UserService {
+public class UserService extends BaseService<User, String, UserRepository> {
 
 	@Autowired
-	private UserRepository userRepository;
+	private BCryptPasswordEncoder passwordEncoder;
 
 	/**
 	 * @param userName
 	 * @return
 	 */
 	public User findByUsername(String userName) {
-		return userRepository.findByUsername(userName);
+		return repository.findByUsername(userName);
+	}
+
+	public void save(User user) {
+		user.setPassword(passwordEncoder.encode(user.getPassword()));
+		super.save(user);
 	}
 
 }
